5217dbe672876e645a3ab9c4f99b6dbae3ac15cc
rulamon <ruben.lamon@gmail.com>
2018-11-25 12:54:22 +0100

    first solution

---
+++ first.js
@@ -0,0 +1,9 @@
+	function destroyer(arr) {
+		let newArr = arguments[0];
+		for (let i = 1; i < arguments.length; i ++) {
+			newArr = newArr.filter(x => x != arguments[i])
+		}
+		return newArr;
+	}
+
+	console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
